e9b21f6f7c2cadff690ddbf503173412724da196
um-ma <umgo0510@gmail.com>
2021-02-12 13:56:37 +0000

    Data added

--- data.js
+++ data.js
@@ -1,11 +1,60 @@
-const data = [
-  {
-      "id": 1, 
-      "name": "testOne"
-  }, 
-  {
-      "id": 2, 
-      "name": "testTwo"
-  }
+const data = {
+  "runs": [
+    {
+      "name": "Run One",
+      "date": "06/01/2021",
+      "time": "19:11:59",
+      "distance": 100,
+      "walk": 30,
+      "powerWalk": 30,
+      "run": 40
+    },
+    {
+      "name": "Run Two",
+      "date": "11/01/2021",
+      "time": "08:22:56",
+      "distance": 30,
+      "walk": 10,
+      "powerWalk": 5,
+      "run": 15
+    },
+    {
+      "name": "Run Three",
+      "date": "16/01/2021",
+      "time": "13:20:16",
+      "distance": 50,
+      "walk": 30,
+      "powerWalk": 20,
+      "run": 0
+    },
+    {
+      "name": "Run Four",
+      "date": "17/01/2021",
+      "time": "15:23:44",
+      "distance": 33,
+      "walk": 11,
+      "powerWalk": 14,
+      "run": 8
+    },
+    {
+      "name": "Run Five",
+      "date": "23/01/2021",
+      "time": "15:43:43",
+      "distance": 64,
+      "walk": 30,
+      "powerWalk": 30,
+      "run": 4
+    },
+    {
+      "name": "Run Six",
+      "date": "09/02/2021",
+      "time": "12:02:58",
+      "distance": 138,
+      "walk": 50,
+      "powerWalk": 70,
+      "run": 18
+    }
   ]
+}
+
   module.exports = data;
